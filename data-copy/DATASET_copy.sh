bf4f0f6a431abffeb60819b7234b316edf3fb9d7
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2023-12-15 16:28:04 +0000

    seeding data-copy

---
+++ data-copy/DATASET_copy.sh
@@ -0,0 +1,3 @@
+cp -rp /u5/mb/XYZ_Oracle_gagnagrunnur data-copy/. &
+cp -rp /u5/mb/prj/LHG data-copy/. &
+cp -rp /net/hafkaldi.hafro.is/export/u3/haf/Julian/bathymetry/LHG_single_beam.csv data-copy/julian/.
